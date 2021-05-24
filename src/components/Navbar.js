376bb361d37247cf304bf3b7da016e5d27495be5
enoralecuyer <48727972+enoralecuyer@users.noreply.github.com>
2021-05-24 14:45:41 -0700

    created nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,41 @@
 import React, { Component } from "react";
+import logo from "../images/logo.png";
+import { FaAlignRight, FaCentercode, FaHiking } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
+  state = {
+    isOpen: false,
+  };
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
   render() {
-    return <div>hello from navbar</div>;
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="mountain logo" className="logo" />
+            </Link>
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+              <Link to="/parks">Parks</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
