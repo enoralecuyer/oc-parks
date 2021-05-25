32425ac6b8fe020d6333a573ac54460de6b7bea5
enoralecuyer <48727972+enoralecuyer@users.noreply.github.com>
2021-05-25 14:51:14 -0700

    created park list

--- src/components/ParkList.js
+++ src/components/ParkList.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Parks from "./Parks";
 
-export default function ParkList() {
-  return <div>hello from parks list</div>;
+export default function ParkList({ parks }) {
+  if (parks.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>Unfortunately, no parks matched your search parameters.</h3>
+      </div>
+    );
+  }
+  return (
+    <section className="parkslist">
+      <div className="parkslist-center">
+        {parks.map((item) => {
+          return <Parks key={item.id} park={item} />;
+        })}
+      </div>
+    </section>
+  );
 }
