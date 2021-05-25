a153fb2f9ff790a955dd3ead9f555f05672b52a1
enoralecuyer <48727972+enoralecuyer@users.noreply.github.com>
2021-05-25 15:18:59 -0700

    added filtering by unique type of parks

--- src/components/ParkFilter.js
+++ src/components/ParkFilter.js
@@ -1,5 +1,59 @@
 import React from "react";
+import { useContext } from "react";
+import { ParkContext } from "../context";
+import Title from "../components/Title";
 
-export default function ParkFilter() {
-  return <div>hello from parks filter</div>;
+// get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+export default function ParkFilter({ parks }) {
+  const context = useContext(ParkContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    size,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+  // get unique types
+  let types = getUnique(parks, "type");
+  // add all
+  types = ["all", ...types];
+  // map to jsx
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="search parks" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">park type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end of select type */}
+      </form>
+    </section>
+  );
 }
--- src/context.js
+++ src/context.js
@@ -8,13 +8,31 @@ class ParkProvider extends Component {
     parks: [],
     sortedParks: [],
     featuredParks: [],
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
   // getData
 
   componentDidMount() {
     let parks = this.formatData(items);
     let featuredParks = parks.filter((park) => park.featured === true);
-    this.setState({ parks, featuredParks, sortedParks: parks });
+    let maxPrice = Math.max(...parks.map((item) => item.price));
+    let maxSize = Math.max(...parks.map((item) => item.size));
+    this.setState({
+      parks,
+      featuredParks,
+      sortedParks: parks,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
+    });
   }
 
   formatData(items) {
@@ -32,9 +50,26 @@ class ParkProvider extends Component {
     return park;
   };
 
+  handleChange = (event) => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(type, name, value);
+  };
+
+  filterParks = () => {
+    console.log("hello");
+  };
+
   render() {
     return (
-      <ParkContext.Provider value={{ ...this.state, getPark: this.getPark }}>
+      <ParkContext.Provider
+        value={{
+          ...this.state,
+          getPark: this.getPark,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </ParkContext.Provider>
     );
