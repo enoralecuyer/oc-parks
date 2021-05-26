3155eb9957befe674dc099626709945f797ce37a
enoralecuyer <48727972+enoralecuyer@users.noreply.github.com>
2021-05-26 13:07:43 -0700

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h3>&copy; {new Date().getFullYear()} OC Hikes. All Rights Reserved</h3>
+    </div>
+  );
+}
+
+export default Footer;
